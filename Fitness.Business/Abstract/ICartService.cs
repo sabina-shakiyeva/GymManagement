7248956aa1a142e007f8b8ec0346993772dc7049
sabina-shakiyeva <shakiyevasabina@gmail.com>
2025-04-24 00:00:31 +0400

    updates

---
+++ Fitness.Business/Abstract/ICartService.cs
@@ -0,0 +1,17 @@
+﻿using Fitness.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.Business.Abstract
+{
+    public interface ICartService
+    {
+        Task AddToCartAsync(int userId, int productId, int quantity);
+        Task RemoveFromCartAsync(int userId, int productId);
+        Task UpdateQuantityAsync(int userId, int productId, int newQuantity);
+        Task<List<CartItem>> GetUserCartAsync(int userId);
+    }
+}
--- Fitness.Entities/Concrete/PurchaseHistory.cs
+++ Fitness.Entities/Concrete/PurchaseHistory.cs
@@ -1,5 +1,4 @@
-﻿using Fitness.Core.Abstraction;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,11 +6,13 @@ using System.Threading.Tasks;
 
 namespace Fitness.Entities.Concrete
 {
-	public class CartItem:IEntity
+	public class PurchaseHistory
 	{
 		public int Id { get; set; }
 		public int UserId { get; set; }
 		public int ProductId { get; set; }
 		public int Quantity { get; set; }
+		public int TotalPoints { get; set; }
+		public DateTime PurchasedAt { get; set; }
 	}
 }
